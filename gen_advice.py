abe4e197c71d0fdca038b0d4d9a258af916912c4
MattUebel <mattuebel@github.com>
2023-10-19 11:07:12 -0400

    add openai call to generate workout advice

---
+++ gen_advice.py
@@ -0,0 +1,28 @@
+import json
+import os
+
+import openai
+
+# Load your OpenAI API key from an environment variable or secret management service
+openai.api_key = os.environ["OPENAI_KEY"]
+
+# Read in the workout plan from the file
+with open("workout_output.txt", "r") as f:
+    workout_text = f.read()
+
+# Create the conversation payload
+payload = {
+    "engine": "gpt-4",  # Using GPT-4
+    "prompt": f"Hello my Personal Trainer! This is my workout plan for the week:\n{workout_text}\n\nCould you help with advice on the exercises for each day? Please respond with markdown formatted text",
+    "max_tokens": 300
+}
+
+# Make the API call
+response = openai.Completion.create(**payload)
+
+# Extract and print the generated advice
+advice = response.choices[0].text.strip()
+
+# Write the advice to a markdown file
+with open("workout_advice.md", "w") as f:
+    f.write(f"## Personal Trainer Advice\n\n{advice}")

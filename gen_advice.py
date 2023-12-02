85aff106866d3fee056551b8781b30f0ec3c1903
MattUebel <mattuebel@github.com>
2023-12-02 15:22:30 -0500

    rework for openai 1.0

--- gen_advice.py
+++ gen_advice.py
@@ -1,26 +1,22 @@
-import json
-import os
+from openai import OpenAI
 
-import openai
+client = OpenAI(api_key=os.environ["OPENAI_KEY"])
 
-# Load your OpenAI API key from an environment variable or secret management service
-openai.api_key = os.environ["OPENAI_KEY"]
-
-# Read in the workout plan from the file
+# Read the workout plan from the file
 with open("workout_output.txt", "r") as f:
     workout_text = f.read()
 
-# Create the conversation payload
-payload = {
-    "model": "gpt-3.5-turbo",
-    "messages": [{"role": "user", "content": f"You are a personal trainer. You are helping a client with their weekly workout plan. Please provide advice for the exercises in each day. Here is the plan:\n\n{workout_text}\n\n"}],
-}
+# Create the conversation prompt
+prompt = f"You are a personal trainer. You are helping a client with their weekly workout plan. Please provide advice for the exercises in each day: \n\n{workout_text}\n\n"
 
-# Make the API call
-response = openai.ChatCompletion.create(**payload)
+# Generate advice using the ChatCompletion API
+completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": prompt}],
+)
 
-# Extract and print the generated advice
-advice = response['choices'][0]['message']['content']
+# Extract the generated advice
+advice = completion.choices[0].message.content
 
 # Write the advice to a markdown file
 with open("workout_advice.md", "w") as f:
--- gen_advice_response.py
+++ gen_advice_response.py
@@ -2,20 +2,17 @@ import json
 import os
 import requests
 
-import openai
+from openai import OpenAI
 
-# Load your OpenAI API key and GitHub token from environment variables
-openai.api_key = os.environ["OPENAI_KEY"]
+client = OpenAI(api_key=os.environ["OPENAI_KEY"])
+
 github_token = os.environ["GITHUB_TOKEN"]
 
 # The URL of the issue. You'd dynamically fetch this in your workflow
 issue_url = os.environ["ISSUE_URL"]
 
 # Get the issue details
-response = requests.get(
-    issue_url,
-    headers={"Authorization": f"token {github_token}"}
-)
+response = requests.get(issue_url, headers={"Authorization": f"token {github_token}"})
 issue_data = response.json()
 
 # The body of the issue contains the workout details
@@ -24,22 +21,18 @@ workout_text = issue_data["body"]
 # The new comment can be obtained from the GitHub event payload, or you could fetch it in a similar way as the issue
 comment_text = os.environ["COMMENT_TEXT"]
 
-# Create the conversation payload
-payload = {
-    "model": "gpt-3.5-turbo",
-    "messages": [
-        {"role": "user", "content": f"You are a personal trainer. You are helping a client with their weekly workout plan. For context, here is the plan:\n\n{workout_text}\n\n"},
-        {"role": "user", "content": f"The client also had the following question or comment:\n\n{comment_text}\n\n"},
-        {"role": "user", "content": f"Please continue the conversation with helpful and detailed advice for the client.\n\n"}
-    ],
-}
+# Build the full conversation prompt
+prompt = f"""You are a personal trainer. You are helping a client with their weekly workout plan. For context, here is the plan:\n\n{workout_text}\n\nThe client also had the following question or comment:\n\n{comment_text}\n\nPlease continue the conversation with helpful and detailed advice for the client.\n\n"""
 
-# Make the API call
-response = openai.ChatCompletion.create(**payload)
+# Generate trainer advice using ChatCompletion
+completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": prompt}],
+)
 
 # Extract the generated advice
-advice = response['choices'][0]['message']['content']
+advice = completion.choices[0].message.content
 
-# Write the advice to a markdown file to be used in the GitHub action to comment back
+# Write the advice to a markdown file for the GitHub action comment
 with open("comment_advice.md", "w") as f:
     f.write(f"## Personal Trainer Response\n\n{advice}")
